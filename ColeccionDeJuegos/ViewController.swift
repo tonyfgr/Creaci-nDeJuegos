ce86b8b8cd5b1086dad190ba272a4b80913b5cbe
tonyfgr <antony.gonzales@tecsup.edu.pe>
2024-05-06 14:40:51 -0500

    Initial Commit

---
+++ ColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Antony Gonzales on 6/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

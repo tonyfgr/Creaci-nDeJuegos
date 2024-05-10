ecfb4ab4da20e69b5c99c7e5172d13bafc669dae
tonyfgr <antony.gonzales@tecsup.edu.pe>
2024-05-10 14:14:15 -0500

    Integracion de mostrar Imagenes en tableView principal

---
+++ ColeccionDeJuegos/JuegosViewController.swift
@@ -0,0 +1,60 @@
+//
+//  JuegosViewController.swift
+//  ColeccionDeJuegos
+//
+//  Created by Antony Gonzales on 6/05/24.
+//
+
+import UIKit
+
+class JuegosViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tituloTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func fotosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true,
+            completion: nil)
+    }
+    @IBAction func camaraTapped(_ sender: Any) {
+    }
+    @IBAction func agregarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as!
+                       AppDelegate).persistentContainer.viewContext
+        let juego = Juego(context: context)
+        juego.titulo = tituloTextField.text
+        juego.imagen = imageView.image?.jpegData(compressionQuality: 0.50)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        let imagenSeleccionada = info[.originalImage] as? UIImage
+    
+        imageView.image = imagenSeleccionada
+        imagePicker.dismiss(animated: true,completion: nil)
+        
+    }
+
+}
--- ColeccionDeJuegos/ViewController.swift
+++ ColeccionDeJuegos/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  ColeccionDeJuegos
-//
-//  Created by Antony Gonzales on 6/05/24.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return juegos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let juego = juegos[indexPath.row]
+        cell.textLabel?.text = juego.titulo
+        cell.imageView?.image = UIImage(data: (juego.imagen!))
+        return cell
+    }
+    
 
+    @IBOutlet weak var tableview: UITableView!
+    var juegos : [Juego] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableview.dataSource = self
+        tableview.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do{
+            try juegos = context.fetch(Juego.fetchRequest())
+            tableview.reloadData()
+        }catch{
     }
 
 
+    }
+    
 }
 
